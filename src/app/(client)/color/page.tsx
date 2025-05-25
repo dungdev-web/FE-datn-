fe84c637304df6d9a5e97008d29c3bcfcb703aaa
baolcfrontend <baolcfrontend@gmail.com>
2025-05-25 18:30:59 +0700

    25/5/2025

---
+++ src/app/(client)/color/page.tsx
@@ -0,0 +1,9 @@
+export default function ColorPage() {
+  return (
+    <div>
+      <h1>Color Page</h1>
+      <p>This is the color page of the application.</p>
+      <div className="w-100 h-100 bg-[#4bd963]"></div>
+    </div>
+  );
+}
