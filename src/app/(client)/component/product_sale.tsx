ff18f0d6516ef0d916d1ab913487e392cc2e5d8e
tantruc69 <tantrucfpt03@gmail.com>
2025-06-02 08:35:45 +0700

    Update product_sale.tsx

--- src/app/(client)/component/product_sale.tsx
+++ src/app/(client)/component/product_sale.tsx
@@ -23,7 +23,7 @@ export default function ProductSale() {
           slidesPerView: 2,
         },
         992: {
-          slidesPerView: 3,
+          slidesPerView: 4,
         }
       }}
       className="product-slider-track"
