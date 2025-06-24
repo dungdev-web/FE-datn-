d615c8fa39c06ceccf898a092c71758a20ef4e70
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-06-24 21:23:28 +0700

    up

---
+++ src/mocks/mockCart.ts
@@ -0,0 +1,60 @@
+// mocks/mockCart.ts
+import { ICartItem,ICart } from "@/types/cart";
+const defaultCart: ICart[] = [
+  {
+    carts_id: 1,
+    user_id: 2,
+    created_at: "2025-06-22T10:00:00.000Z",
+    updated_at: "2025-06-22T10:00:00.000Z",
+    items: [
+      {
+        cart_items_id: 1,
+        cart_id: 1,
+        variant_id: 101,
+        quantity: 2,
+        price: 1500000,
+        created_at: "2025-06-22T10:00:00.000Z",
+        updated_at: "2025-06-22T10:00:00.000Z",
+      },
+      {
+        cart_items_id: 2,
+        cart_id: 1,
+        variant_id: 102,
+        quantity: 1,
+        price: 2000000,
+        created_at: "2025-06-22T10:00:00.000Z",
+        updated_at: "2025-06-22T10:00:00.000Z",
+      }
+    ]
+  },
+  {
+    carts_id: 2,
+    user_id: 2,
+    created_at: "2025-06-22T11:00:00.000Z",
+    updated_at: "2025-06-22T11:00:00.000Z",
+    items: [
+      {
+        cart_items_id: 3,
+        cart_id: 2,
+        variant_id: 103,
+        quantity: 3,
+        price: 1200000,
+        created_at: "2025-06-22T11:00:00.000Z",
+        updated_at: "2025-06-22T11:00:00.000Z",
+      }
+    ]
+  }
+];
+export const getMockCart = (): ICart[] => {
+  if (typeof window !== "undefined") {
+    const stored = localStorage.getItem("mockCarts");
+    return stored ? JSON.parse(stored) : defaultCart;
+  }
+  return defaultCart;
+};
+
+export const saveMockCart = (carts: ICart[]) => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem("mockCarts", JSON.stringify(carts));
+  }
+};
