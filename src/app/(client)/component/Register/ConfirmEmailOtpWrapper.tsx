ac865462dcc5d65e34b6cb17aea161864becb502
tantruc69 <tantrucfpt03@gmail.com>
2025-08-05 22:46:07 +0700

    update

--- src/app/(client)/component/Register/ConfirmEmailOtpWrapper.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import ConfirmEmailOtp from "../ConfirmEmailOtp";
-
-
-export default function ConfirmEmailOtpWrapper() {
-  return <ConfirmEmailOtp />;
-}
--- src/app/(client)/payment_successful/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-import CheckoutSuccess from "../component/PaymentSuccess ";
-export default function Page() {
-  return (
-    <Suspense fallback={<div>Đang tải...</div>}>
-      <CheckoutSuccess />
-    </Suspense>
-  );
-}
