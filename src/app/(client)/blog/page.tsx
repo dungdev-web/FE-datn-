be9ab781d16c74ae1a949f962c3071d735f87686
tantruc69 <tantrucfpt03@gmail.com>
2025-08-06 14:36:40 +0700

    Update page.tsx

--- src/app/(client)/blog/page.tsx
+++ src/app/(client)/blog/page.tsx
@@ -7,7 +7,7 @@ import { IBlog } from "@/types/blog";
 import { getPost } from "@/services/blogService";
 import { API_BASE_URL } from "@/config/env";
 import { getLocalViews, increaseLocalViews } from "@/shared/until/viewTracker";
-import AsideBlog from "../component/Blog/AsideBlog";
+import AsideBlog from "../component/blog/AsideBlog";
 
 import Link from "next/link";
 export default function Blog() {
