acbad331928dc6eb934830033fff6e44f58c454c
baolcfrontend <baolcfrontend@gmail.com>
2025-08-21 15:35:03 +0700

    css hiện icons

--- src/app/(client)/component/products/HotProductIcons.tsx
+++ src/app/(client)/component/products/HotProductIcons.tsx
@@ -5,7 +5,7 @@ import { useAddToCart } from "@/hooks/useAddToCart";
 import { useAddToCompare } from "@/hooks/useAddToCompare";
 
 interface HotProductIconsProps {
- productId: number;
+  productId: number;
   variant: {
     id: number;
     stock_quantity: number;
@@ -15,10 +15,12 @@ interface HotProductIconsProps {
   onWishlistChange?: () => void;
 }
 
-export default function HotProductIcons({  productId,
+export default function HotProductIcons({
+  productId,
   variant,
   price,
-  onWishlistChange,}: HotProductIconsProps) {
+  onWishlistChange,
+}: HotProductIconsProps) {
   const { isWished, handleAddToWishlist } = useAddToWishlist(productId);
   const { handleAddToCart } = useAddToCart();
   const { isCompared, handleAddCompare, loading } = useAddToCompare(productId);
@@ -26,29 +28,32 @@ export default function HotProductIcons({  productId,
   return (
     <div className="hot-product-icons">
       <i
-        className={`fa-solid fa-heart icon-favorite ${isWished ? "active" : ""}`}
+        className={`fa-solid fa-heart icon-favorite ${
+          isWished ? "active" : ""
+        }`}
         onClick={handleAddToWishlist}
       ></i>
 
       <div className="icon-hover-group">
-         <i className="fa-solid fa-eye icon-eye"></i>
+        <i className="fa-solid fa-eye icon-eye"></i>
         <i
           className="fa fa-shopping-bag position-relative"
-            onClick={() =>
+          onClick={() =>
             handleAddToCart({
               variant,
               quantity: 1,
             })
           }
         ></i>
- <button
+        <button
           className={`compare-btn ${isCompared ? "active" : ""}`}
           onClick={handleAddCompare}
           title={isCompared ? "Đã thêm vào so sánh" : "Thêm vào so sánh"}
           disabled={loading}
         >
           <i className="fa fa-exchange" aria-hidden="true"></i>
-        </button>      </div>
+        </button>{" "}
+      </div>
     </div>
   );
 }
