56caab37137d7e997a2251a85b30bd3912615743
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-08-24 15:00:32 +0700

    Update page.tsx

--- src/app/(client)/page.tsx
+++ src/app/(client)/page.tsx
@@ -6,7 +6,7 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { useEffect, useState, useRef, use } from "react";
 
-import BlogHome from "@/app/(client)/component/Blog/AsideBlog";
+import BlogHome from "./component/Home/BlogHome";
 import CouponApp from "@/app/(client)/component/Coupon";
 import FlashSale from "@/app/(client)/component/FlashSale";
 import { IProduct } from "@/types/product";
