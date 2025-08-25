ee07af128dbc41d4cda789597dd20683802a5a91
LƯU ĐỨC DŨNG <157196568+DungCoderk5@users.noreply.github.com>
2025-08-25 14:44:39 +0700

    up

--- src/hooks/useAuthCookie.ts
+++ src/hooks/useAuthCookie.ts
@@ -3,7 +3,7 @@ import { StoredUser, UserData } from "@/types/user";
 
 export function useAuthCookie() {
   const saveUserToCookies = (data: UserData): void => {
-    Cookies.set("token", data.token, { expires: 1 });
+    Cookies.set("token3", data.token, { expires: 1 });
   };
 
   const clearUserFromCookies = (): void => {
