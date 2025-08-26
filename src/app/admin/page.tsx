8cd3bee62cc410fe25932800e9466a1e6de672cb
baolcfrontend <luuducdungdcm@gmail.com>
2025-08-26 11:08:28 +0700

    thêm link

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -26,6 +26,7 @@ import {
   getStatusText,
   formatDate,
 } from "@/services/dashboardService";
+import Link from "next/link";
 
 export default function Home_admin() {
   const [isCollapsed, setIsCollapsed] = useState(false);
@@ -376,17 +377,19 @@ export default function Home_admin() {
           <ul className="notification-list">
             {data.pendingOrders?.data.length > 0 ? (
               data.pendingOrders.data.map((order: any, index: number) => (
-                <li key={index}>
-                  <strong>Đơn #{order.orders_id}</strong> – Khách:{" "}
-                  <em>{order.user.name}</em>
-                  <br />
-                  Ngày đặt:{" "}
-                  {new Date(order.created_at).toLocaleDateString("vi-VN")} –
-                  Tổng tiền:{" "}
-                  <strong>
-                    {order?.total_amount.toLocaleString("vi-VN")}₫
-                  </strong>
-                </li>
+                <Link href="/admin/order">
+                  <li key={index}>
+                    <strong>Đơn #{order.orders_id}</strong> – Khách:{" "}
+                    <em>{order.user.name}</em>
+                    <br />
+                    Ngày đặt:{" "}
+                    {new Date(order.created_at).toLocaleDateString("vi-VN")} –
+                    Tổng tiền:{" "}
+                    <strong>
+                      {order?.total_amount.toLocaleString("vi-VN")}₫
+                    </strong>
+                  </li>
+                </Link>
               ))
             ) : (
               <li>Không có đơn hàng nào đang chờ xác nhận</li>
